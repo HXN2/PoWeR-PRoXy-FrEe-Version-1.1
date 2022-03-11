e17df6f1035a7da04dd5b1d5c89e5a6a4e0958e9
HEXiN @hxn_here  insta <98489657+HXN2@users.noreply.github.com>
2022-03-11 14:59:25 +0300

    Create PRoXy PoWeR Free v1.2.py

---
+++ PRoXy PoWeR Free v1.2.py
@@ -0,0 +1,475 @@
+import os
+
+try:
+
+    import requests
+
+    import socket as s
+
+    import webbrowser
+
+    from sys import platform
+
+    import colorama
+
+except ModuleNotFoundError:
+
+    os.system("pip install requests")
+
+    os.system("pip install socket")
+
+    os.system("pip install webbrowser")
+
+    os.system("pip install sys")
+
+    os.system("pip install platform")
+
+    os.system("pip install colorama")
+
+os.system('clear')
+
+import webbrowser
+
+url = "https://t.me/HEXiN1K"
+
+webbrowser.open(url)
+
+from sys import platform
+
+from colorama import *
+
+import socket
+
+name = socket.gethostname()
+
+myHostName = socket.gethostname()
+
+IP = socket.gethostbyname(myHostName)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+print("\n===========================================================")
+
+name = ("""\n                    Hello sir """ +name+Style.RESET_ALL)
+
+print(name)
+
+host = ("""\n                  Your platform is """+ platform+Style.RESET_ALL)
+
+print(host)
+
+IP = ("""\n                Your Local iP is """+Fore.RED+IP+Style.RESET_ALL)
+
+print(IP)
+
+v = (Fore.RED+"""\n                    Your iNFO PROXY is\n"""+Style.RESET_ALL)
+
+print(v)
+
+print(s)
+
+print("\n===========================================================")
+
+lib = input("""
+
+\n  من صنع هيكسن انستجرام @hxn_here
+
+\n  BY HEXiN INSTA @hxn_here
+
+\n  tarafından yapılmış HEXiN Instagram @hxn_here
+
+\n
+
+===========================================================
+
+\n   من صنع هيكسن تيليجرام @HEXiN 
+
+\n  BY HEXiN TELeGRaM @HEXiN1K
+
+\n  Bir telgraf HEXiN oluşturun @HEXiN1K
+
+\n ===========================================================
+
+ 
+
+ [1] DOWNLOAD LiP & UPDATE . تنزيل وتحديث المكاتب
+
+\n [2] SKiP DOWNLOAD & UPDATE LiP . تخطي تنزيل وتحديث المكاتب 
+
+\n [+] Please Choice >> """)
+
+if lib == "1":
+
+    try:
+
+        os.system('pip install requests')
+
+        os.system('pip install webbrowser')
+
+        os.system('pip install get')
+
+        os.system('pip install re')  
+
+        os.system('pip install findall')
+
+        os.system('pip install socket')
+
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+        pass
+
+    except:
+
+        pass
+
+else:
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    pass
+
+os.system('clear')
+
+import requests
+
+import webbrowser
+
+import socket
+
+from requests import get
+
+from re import findall
+
+print("""
+
+                _    ___   ___   _
+
+               | |  | \ \ / / \ | |
+
+               | |__| |\ V /|  \| |
+
+               |  __  | > < | . ` |
+
+               | |  | |/ . \| |\  |
+
+               |_|  |_/_/ \_\_| \_|
+
+  _ __  _ __ _____  ___   _
+
+ | '_ \| '__/ _ \ \/ / | | |
+
+ | |_) | | | (_) >  <| |_| |          Version 1.1
+
+ | .__/|_|  \___/_/\ _\\__, |
+
+ | |                   __/ |
+
+ |_|                  |___/       
+
+              
+
+              Daddy HEXiN is here @hxn_here
+
+ To see the latest Version of this tool, go to my channel     in a Telegram @HEXiN1K
+
+ """)
+
+from requests import get
+
+from re import findall
+
+import time as mm
+
+import sys as n
+
+def slow(M):
+
+	for c in M + '\n':		n.stdout.write(c)
+
+		n.stdout.flush()
+
+		mm.sleep(1. / 160)
+
+slow(""" \n Buy the special edition, head over to  Instagram @hxn_here  Good Bye.
+
+لشراء النسخة الخاصة توجة الى انستجرام @hxn_here الى اللقاء . """)
+
+S = input("""
+
+ 
+
+ ~ [1] Do you want to start searching for proxy? . 
+
+ هل تريد بدء البحث عن بروكسي ؟
+
+ ~ [2] Do you want Follow HEXiN in iNStAGrAM Baby ? .
+
+  هل تريد متابعة هيكسين على انستجرام بيبي ؟
+
+ 
+
+ [+] إختيارك رجاءً
+
+ [+] Please Choose: """)
+
+if S =='1':
+
+ from requests import get
+
+from re import findall
+
+urls = '''
+
+http://free-proxy.cz/en/proxylist/country/all/https/ping/level2
+
+https://www.proxy-list.download/HTTP
+
+https://premiumproxy.net/
+
+https://www.proxyrack.com/free-proxy-list/
+
+https://advanced.name/freeproxy
+
+https://vpnoverview.com/privacy/anonymous-browsing/free-proxy-servers/
+
+https://free-proxy-list.com/
+
+https://www.proxyscan.io/
+
+https://openproxy.space/list/socks5
+
+https://openproxy.space/list/socks4
+
+https://openproxy.space/list/http
+
+https://www.sslproxies.org/
+
+https://proxyscrape.com/free-proxy-list
+
+https://www.us-proxy.org/
+
+https://www.proxynova.com/proxy-server-list/
+
+https://premproxy.com/list/
+
+https://proxyservers.pro/proxy/list/speed/2/order/updated/order_dir/desc/page/1
+
+https://proxyservers.pro/proxy/list/speed/2/anonymity/anonymous/order/updated/order_dir/desc/page/1
+
+https://premproxy.com/list/06.htm
+
+https://premproxy.com/list/05.htm
+
+https://premproxy.com/list/04.htm
+
+https://premproxy.com/list/02.htm
+
+https://premproxy.com/list/03.htm
+
+https://premproxy.com/list/
+
+https://proxyservers.pro/proxy/list/speed/2/order/updated/order_dir/desc/page/1
+
+https://proxyservers.pro/proxy/list/speed/2/anonymity/anonymous/order/updated/order_dir/desc/page/1
+
+https://fineproxy.de/en/free-proxy-server-list/
+
+http://free-proxy.cz/en/proxylist/main/15
+
+https://secure.echolink.org/proxylist.jsp
+
+http://free-proxy.cz/en/proxylist/main/14
+
+https://www.proxyscan.io/
+
+https://proxy-daily.com/
+
+http://free-proxy.cz/en/proxylist/main/13
+
+http://free-proxy.cz/en/proxylist/main/12
+
+http://free-proxy.cz/en/proxylist/main/2
+
+http://free-proxy.cz/en/proxylist/main/4
+
+http://free-proxy.cz/en/proxylist/main/5
+
+http://free-proxy.cz/en/proxylist/main/7
+
+http://free-proxy.cz/en/proxylist/main/6
+
+http://free-proxy.cz/en/proxylist/main/8
+
+http://free-proxy.cz/en/proxylist/main/9
+
+http://free-proxy.cz/en/proxylist/main/10
+
+http://free-proxy.cz/en/proxylist/main/3
+
+http://free-proxy.cz/en/proxylist/main/11
+
+http://free-proxy.cz/en/
+
+https://fineproxy.de/en/free-proxy-server-list/
+
+https://premproxy.com/list/
+
+https://proxyservers.pro/proxy/list/speed/2/order/updated/order_dir/desc/page/1
+
+https://proxyservers.pro/proxy/list/speed/2/anonymity/anonymous/order/updated/order_dir/desc/page/1
+
+https://proxy-list.org/english/index.php
+
+https://proxypremium.top/
+
+https://proxypremium.top/search-proxy
+
+https://www.locaproxy.com/server-list
+
+https://squidproxyserver.com/
+
+https://premproxy.com/list/10.htm
+
+https://fineproxy.de/en/free-proxy-server-list/
+
+http://free-proxy.cz/en/proxylist/main/15
+
+https://secure.echolink.org/proxylist.jsp
+
+http://free-proxy.cz/en/proxylist/main/14
+
+http://free-proxy.cz/en/proxylist/country/all/https/ping/all
+
+https://www.proxy-list.download/HTTPS
+
+http://proxydb.net/
+
+http://atomintersoft.com/transparent_proxy_list
+
+/
+
+https://free-proxy-list.com/
+
+'''
+
+total = 0
+
+headers = {
+
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36',
+
+}
+
+if S =='2':
+
+  os.system('clear')
+
+import webbrowser
+
+url = "https://www.instagram.com/hxn_here/"
+
+webbrowser.open(url)
+
+print("""
+
+                _    ___   ___   _
+
+               | |  | \ \ / / \ | |
+
+               | |__| |\ V /|  \| |
+
+               |  __  | > < | . ` |
+
+               | |  | |/ . \| |\  |
+
+               |_|  |_/_/ \_\_| \_|
+
+  _ __  _ __ _____  ___   _
+
+ | '_ \| '__/ _ \ \/ / | | |
+
+ | |_) | | | (_) >  <| |_| |
+
+ | .__/|_|  \___/_/\ _\\__, |
+
+ | |                   __/ |
+
+ |_|                  |___/
+
+              
+
+              Daddy HEXiN is here @hxn_here
+
+               Thx Baby S H , cdiw 
+
+""")
+
+with open('proxy.txt', 'w') as proxy_file:
+
+    proxy_file.write('Proxy with file By Daddy HEXiN @hxn_here << insta \n\n')
+
+    proxy_file.close()
+
+for u in urls.splitlines():
+
+    try:
+
+        if u == '':
+
+            pass
+
+        else:
+
+            res = get(str(u),headers=headers)
+
+            proxies = findall(r'\d\d?\d?\.\d\d?\d?\.\d\d?\d?\.\d\d?\d?:\d\d\d?\d?\d?',res.text)
+
+            _len = (len(proxies))
+
+            if int(_len) == 0:
+
+                print(f' Scrapping from: [ {u} ]')
+
+                ip = findall(r'\d\d?\d?\.\d\d?\d?\.\d\d?\d?\.\d\d?\d?', res.text)
+
+                port = findall(r':\d\d\d?\d?\d?', res.text)
+
+                _len2 = len(ip)
+
+                total += _len2
+
+                print(f' New {_len2} proxy scrapped\n')
+
+                for proxy in range(_len2):
+
+                    with open('proxy.txt','a') as proxy_file:
+
+                        try:
+
+                            proxy_file.write(str(ip[proxy]+port[proxy])+'\n')
+
+                        except:
+
+                            pass
+
+            else:
+
+                print(f' Scrapping from: [ {u} ]')
+
+                total += _len
+
+                print(f' New {_len} proxy scrapped\n')
+
+                for _proxy in proxies:
+
+                    with open('proxy.txt', 'a') as proxy_file:
+
+                        proxy_file.write(str(_proxy) + '\n')
+
+    except:
+
+        print(f' Error while scraping [ {u} ]\n')
+
+        pass
+
+input(f' Done Scrapping! total proxies: {total}')
